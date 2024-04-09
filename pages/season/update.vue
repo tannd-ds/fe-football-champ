a67e75d7c9fe5b3c82696c0033bec60f60d12ef1
tannd-ds <donhattan1012dev@gmail.com>
2024-04-09 17:46:00 +0700

    Add advanced settings to add/update season page

--- pages/season/update.vue
+++ pages/season/update.vue
@@ -45,6 +45,121 @@
           required
         />
 
+        <UAccordion :items="advanced_settings">
+          <template #default="{ item, index, open }">
+            <UButton color="gray" variant="ghost" class="rounded-md" :ui="{ rounded: 'rounded-none', padding: { sm: 'p-3' } }">
+              <span class="text-md text-zinc-300">{{ item.label }}</span>
+
+              <template #trailing>
+                <UIcon
+                  name="i-heroicons-chevron-right-20-solid"
+                  class="w-5 h-5 ms-auto transform transition-transform duration-200"
+                  :class="[open && 'rotate-90']"
+                />
+              </template>
+            </UButton>
+          </template>
+
+          <template #advanced>
+            <div class="flex flex-col gap-3" >
+
+              <!-- Tuổi cầu thủ -->
+              <div class="w-full flex gap-4 items-stretch">
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.min_age"
+                  label="Tuổi Cầu Thủ Tối Thiểu"
+                  name="min_age"
+                  input-type="number"
+                />
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.max_age"
+                  label="Tuổi Cầu Thủ Tối Đa"
+                  name="max_age"
+                  input-type="number"
+                />
+              </div>
+              
+              <!-- Số lượng cầu thủ -->
+              <div class="w-full flex gap-4 items-stretch">
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.min_quantity_soccer"
+                  label="Số lượng Cầu Thủ Tối Thiểu"
+                  name="min_quantity_soccer"
+                  input-type="number"
+                />
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.max_quantity_soccer"
+                  label="Số lượng Cầu Thủ Tối Đa"
+                  name="max_quantity_soccer"
+                  input-type="number"
+                />
+
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.max_quantity_foreign_soccer"
+                  label="SL Cầu Thủ Nước Ngoài Tối Đa"
+                  name="max_quantity_foreign_soccer"
+                  input-type="number"
+                />
+              </div>
+
+              <!-- Số lượng bàn thắng -->
+              <CInput
+                v-model="advanced_state.quantity_category_goal"
+                label="Số Loại Bàn Thắng"
+                name="quantity_category_goal"
+                input-type="number"
+              />
+
+              <!-- Thời gian trận đấu -->
+              <CInput
+                v-model="advanced_state.max_time_match"
+                label="Thời Gian Trận Đấu (phút)"
+                name="max_time_match"
+                input-type="number"
+              />
+
+              <!-- Điểm số -->
+              <div class="w-full flex gap-4 items-stretch">
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.win_score"
+                  label="Điểm Số Thắng"
+                  name="win_score"
+                  input-type="number"
+                />
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.draw_score"
+                  label="Điểm Số Hòa"
+                  name="draw_score"
+                  input-type="number"
+                />
+                <CInput
+                  class="grow"
+                  v-model="advanced_state.lose_score"
+                  label="Điểm Số Thua"
+                  name="lose_score"
+                  input-type="number"
+                />
+              </div>
+
+              <!-- Sắp xếp -->
+              <CInput
+                v-model="advanced_state.category_sort"
+                label="Cách Xếp Hạng"
+                name="category_sort"
+                input-type="number"
+              />
+
+            </div>
+          </template>
+        </UAccordion>
+
         <div>
           <UButton size="xl" type="submit">Submit</UButton>
         </div>
@@ -71,6 +186,28 @@ const state = ref({
   quantity_team: '',
 });
 
+const advanced_state = ref({
+  min_age: '',
+  max_age: '',
+  min_quantity_soccer: '',
+  max_quantity_soccer: '',
+  max_quantity_foreign_soccer: '',
+  quantity_category_goal: '',
+  max_time_match: '',
+  win_score: '',
+  draw_score: '',
+  lose_score: '',
+  category_sort: '',
+})
+
+const advanced_settings = [
+  {
+    label: 'Nâng Cao',
+    icon: 'i-heroicons-information-circle',
+    slot: 'advanced',
+  }
+]
+
 if (route.query.season_id) {
   PAGE_TITLE = 'Chỉnh Sửa Mùa Giải';
   fetch_api = 'http://localhost:8000/api/season/update/' + route.query.season_id;
@@ -79,6 +216,19 @@ if (route.query.season_id) {
 
   for (const key in state.value) {
     state.value[key] = String(res.data.value[0][key]);
+  }
+
+  function time_to_minutes(time) {
+    const [hours, minutes] = time.split(':');
+    return String(parseInt(hours) * 60 + parseInt(minutes));
+  }
+
+  for (const key in advanced_state.value) {
+    if (key == 'max_time_match') {
+      advanced_state.value[key] = time_to_minutes(res.data.value[0][key]);
+      continue;
+    }
+    advanced_state.value[key] = String(res.data.value[0][key]);
   }
 }
 
@@ -94,6 +244,23 @@ useHead({
 
 async function handleSubmit() {
   try {
+
+    function minutes_to_time(minutes) {
+      const hours = Math.floor(minutes / 60);
+      const mins = minutes % 60;
+      return `${hours}:${mins}:00`;
+    }
+
+    // Add advanced settings to state if it's not empty
+    for (const key in advanced_state.value) {
+      if (advanced_state.value[key] == '')
+        continue;
+      if (key == 'max_time_match') {
+        state.value[key] = minutes_to_time(advanced_state.value[key]);
+      }
+      state.value[key] = advanced_state.value[key];
+    }
+
     const response = await useFetch(fetch_api, {
       method: 'POST',
       headers: {

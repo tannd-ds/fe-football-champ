d6caf3fde6648ff90cf4fd38e1965d257e34b1af
tannd-ds <donhattan1012dev@gmail.com>
2024-04-25 14:30:47 +0700

    design: update CMatchItem

--- components/C/match/item.vue
+++ components/C/match/item.vue
@@ -1,34 +1,41 @@
 <template>
   <div
-    class="w-full p-4 bg-zinc-900 hover:bg-zinc-800 rounded-lg 
-        border border-zinc-700
-        cursor-pointer transition-all duration-200 ease-in-out"
+    class="w-full p-4 bg-zinc-900 rounded-lg 
+        border border-zinc-800
+        transition-all duration-200 ease-in-out"
   >
-    <div class="w-full justify-between items-center grid grid-cols-10">
-      <div class="ml-4 col-span-1 flex flex-col items-center">
+    <div class="w-full justify-between items-center grid grid-cols-11">
+      <div class="ml-4 col-span-2 flex flex-col items-start">
         <span class="font-bold text-lg">{{ match.time }}</span>
         <span class="text-zinc-400 text-sm">{{ match.date_only }}</span>
       </div>
 
-      <div class="col-span-2 flex flex-col gap-1 justify-center items-center">
+      <button 
+        class="unset col-span-2 flex flex-col gap-1 justify-center items-center"
+        :title="`Xem thông tin ${match.team_1_name}`"
+        @click="router.push(`/team/${match.team_id_1}`)"
+      >
         <LazyUAvatar 
           :src="`http://localhost:8000/api/get_img/team__${match.team_1_url_image}`"
           alt="team_1_logo" 
           class="w-10 h-10"
         />
         <div class="font-bold text-sm text-center">{{ match.team_1_name }}</div>
-      </div>
+      </button>
 
       <div class="font-bold col-span-1 text-center">VS</div>
 
-      <div class="col-span-2 flex flex-col gap-1 justify-center items-center">
+      <button class="unset col-span-2 flex flex-col gap-1 justify-center items-center"
+        :title="`Xem thông tin ${match.team_2_name}`"
+        @click="router.push(`/team/${match.team_id_2}`)"
+      >
         <LazyUAvatar 
           :src="`http://localhost:8000/api/get_img/team__${match.team_2_url_image}`"
           alt="team_1_logo" 
           class="w-10 h-10"
         />
         <div class="font-bold text-sm text-center">{{ match.team_2_name }}</div>
-      </div>
+      </button>
 
       <div class="col-span-3 flex justify-center">
         <span 
@@ -38,13 +45,13 @@
         <span v-else>{{ match.team1_score }}-{{ match.team2_score }}</span>
       </div>
 
-      <div 
-        class="col-span-1 text-sm text-zinc-200 flex gap-2 items-center justify-end"
+      <button 
+        class="unset col-span-1 text-sm text-zinc-200 flex gap-2 items-center justify-end"
         @click="router.push(`/match/${match.schedule_id}`)"
       >
         <span>Chi Tiết</span>
         <UIcon name="i-heroicons-chevron-right" />
-      </div>
+      </button>
     </div>
   </div>
 </template>

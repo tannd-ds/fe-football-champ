7377edcf6d4a2d1edf7b8747b141316b3d409a4e
tannd-ds <donhattan1012dev@gmail.com>
2024-04-26 22:30:02 +0700

    usable registration manage feature

--- pages/season/[id].vue
+++ pages/season/[id].vue
@@ -1,52 +1,93 @@
 <template>
-  <div class="w-full h-[98vh] grid grid-cols-3 gap-4">
-    <AppCard 
-      class="col-span-2"
-    >
-      <div class="flex flex-col gap-4">
-        <div class="flex justify-between items-center">
-          <div class="space-y-2">
-            <div class="text-3xl font-bold"> {{ season_info.name_season }}</div>
-            <div>Lịch Thi Đấu</div>
+  <div class="w-full">
+    <div class="w-full h-[98vh] grid grid-cols-3 gap-4">
+      <AppCard 
+        class="col-span-2"
+      >
+        <div class="flex flex-col gap-4">
+          <div class="flex justify-between items-center">
+            <div class="space-y-2">
+              <div class="text-3xl font-bold"> {{ season_info.name_season }}</div>
+              <div>Lịch Thi Đấu</div>
+            </div>
+
+            <UButton 
+              color="primary"
+              icon="i-heroicons-calendar-20-solid"
+              label="Lên Lịch"
+              @click="router.push(`/match/update?season_id=${season_id}`)"
+              :disabled="!can_schedule_match"
+            />
           </div>
 
-          <UButton 
-            color="primary"
-            icon="i-heroicons-calendar-20-solid"
-            label="Lên Lịch"
-            @click="router.push(`/match/update?season_id=${season_id}`)"
-            :disabled="!can_schedule_match"
-          />
+          <div class="flex flex-col gap-4">
+            <CMatchItem
+              v-for="(match, match_index) in all_matches_filtered"
+              :match="match"
+            />
+          </div>
         </div>
 
-        <div class="flex flex-col gap-4">
-          <CMatchItem
-            v-for="(match, match_index) in all_matches_filtered"
-            :match="match"
-          />
-        </div>
+      </AppCard>
+
+      <div class="w-full flex flex-col gap-4">
+        <TableBaseViewer 
+          class="shrink"
+          :data="filter_teams" 
+          :columns="teams_columns" 
+          table-name="team"
+        >
+          <template #header>
+            <div>Bảng Xếp Hạng</div>
+          </template>
+        </TableBaseViewer>
+
+        <UButton
+          label="Đơn Đăng Ký"
+          size="lg"
+          icon="i-heroicons-queue-list-20-solid"
+          @click="regis_pannel_is_open = true"
+        />
       </div>
-
-    </AppCard>
-
-    <div class="w-full flex flex-col gap-4">
-      <TableBaseViewer 
-        class="shrink"
-        :data="filter_teams" 
-        :columns="teams_columns" 
-        table-name="team"
-      >
-        <template #header>
-          <div>Bảng Xếp Hạng</div>
-        </template>
-      </TableBaseViewer>
-
-      <UButton
-        label="Đơn Đăng Ký"
-        size="lg"
-        icon="i-heroicons-document-plus-20-solid"
-      />
     </div>
+
+    <UModal v-model="regis_pannel_is_open">
+      <div class="bg-transparent">
+        <TableBaseViewer 
+          class="shrink"
+          :data="all_regis" 
+          :columns="regis_columns" 
+          :items="items"
+          table-name="team"
+        >
+          <template #header>
+            <div>Đơn Đăng Ký chưa Xét Duyệt</div>
+          </template>
+        </TableBaseViewer>
+
+        <UCard
+        v-if="selected_regis"
+        :ui="{
+          base: 'h-full flex flex-col',
+          rounded: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+          body: {
+            base: 'grow'
+          }
+        }"
+        >
+          {{ selected_regis }}
+          <div class="w-full">
+            <UButton
+              @click="utilsRegisAccept(selected_regis)"
+            >Duyệt</UButton>
+            <UButton
+              @click="utilsRegisReject(selected_regis)"
+            >Hủy</UButton>
+          </div>
+        </UCard>
+      </div>
+    </UModal>
   </div>
 </template>
 
@@ -112,6 +153,30 @@ const teams_columns = [
   { key: 'draw', label: 'Hòa'},
   { key: 'lose', label: 'Thua'},
   { key: 'total', label: 'Điểm'}
+]
+
+// REGISTER PANEL ------------------------------
+const regis_pannel_is_open = ref(false);
+
+let all_regis = ref({'data': []});
+all_regis.value = await useFetch(`http://localhost:8000/api/season/get_registration/${season_id}`);
+let selected_regis = ref({});
+
+const regis_columns = [
+  { key: 'url_image', label: '' },
+  { key: 'name_team', label: 'Đội' },
+  { key: 'date_signin', label: 'Ngày Đăng Ký' },
+  { key: 'actions'}
+]
+
+const items = (row) => [
+  [{
+    label: 'Xem Đơn',
+    icon: 'i-heroicons-pencil-square-20-solid',
+    click: () => {
+      selected_regis.value = row;
+    }
+  }]
 ]
 
 </script>

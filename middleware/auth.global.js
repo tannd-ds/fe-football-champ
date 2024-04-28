024d645a9d705697886f4b63c487946df649ddd9
tannd-ds <donhattan1012dev@gmail.com>
2024-04-28 22:38:06 +0700

    implement dynamic routing in auth midleware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -3,6 +3,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const login_page = '/login';
   const regis_page = '/register/account';
   const auth_cookie = useCookie('auth');
+  const { value: usr_info } = useCookie('usr_info');
   if (to.path == login_page || to.path == regis_page) {
     if (auth_cookie.value) {
       return navigateTo('/');
@@ -18,4 +19,19 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (auth_cookie.value == undefined) {
     return navigateTo(login_page);
   }
+
+  if (usr_info.role == 0) {
+    if (!to.path.startsWith('/u/')) {
+      // If user is not admin, 
+      // and user is trying to access admin page, 
+      // redirect to user page
+      return navigateTo(`/u${to.path}`);
+    }
+  } else {
+    if (to.path.startsWith('/u/')) {
+      // Same as above, but for admin
+      return navigateTo(to.path.replace('/u', ''));
+    }
+  }
+
 })

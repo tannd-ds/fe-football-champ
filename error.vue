3b1fa5222c0c7c455f09c8af7476792e5049511b
tannd-ds <donhattan1012dev@gmail.com>
2024-04-29 16:36:33 +0700

    add custom error page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({ error: Object })
+</script>
+
+<template>
+  <NuxtLayout name="error">
+    <div class="flex flex-col gap-4">
+      <!-- <h1 class="text-9xl font-mono">{{ error.statusCode }}</h1> -->
+      <h1 class="text-8xl font-mono text-primary">ÔI KHÔNG...</h1>
+      <div class="text-xl">Bạn không thể truy cập vào trang này...</div>
+      <NuxtLink to="/">
+        <UButton>
+          Về Trang Chủ
+        </UButton>
+      </NuxtLink>
+    </div>
+  </NuxtLayout>
+</template>
---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-2 items-center gap-4">
+    <div class="h-screen flex flex-row items-center justify-center">
+      <slot></slot>
+    </div>
+    <div class="w-full h-full flex items-center justify-center">
+      <img src="/imgs/soccer_illustration.png" />
+    </div>
+  </div>
+  <div class="w-screen h-screen -z-1 absolute top-0 left-0 -z-[1]">
+    <div class="bg-overlay w-full h-full absolute top-0 left-0"></div>
+  </div>
+</template>
+
+<style>
+.bg-overlay {
+  background-color: #000;
+  opacity: 0.3;
+}
+</style>

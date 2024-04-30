8384d214185997e482aa4f27ee4c879ed13ccece
tannd-ds <donhattan1012dev@gmail.com>
2024-04-30 10:15:12 +0700

    update user sidebar design

--- components/Sidebar/UserContainer.vue
+++ components/Sidebar/UserContainer.vue
@@ -3,56 +3,58 @@
     class="py-8 w-[380px] h-screen bg-zinc-900 bg-opacity-90 flex flex-col justify-between items-center"
   >
     
-    <div class="w-full px-8 flex flex-col gap-8">
-      <SidebarLogo />
-    </div>
+    <div class="w-full flex flex-col gap-8">
+      <div class="w-full px-8 flex flex-col gap-8">
+        <SidebarLogo />
+      </div>
 
-    <div class="ml-16 w-full flex flex-col gap-2">
-      <div class="font-light mb-2 text-md">Chung</div>
-      <UButton 
-        size="lg"
-        variant="ghost"
-        color="black" 
-        label="Nhà" 
-        icon="i-heroicons-home-20-solid" 
-        @click="() => router.push('/')"
-        square
-      />
-      <UButton 
-        size="lg"
-        variant="ghost"
-        color="black" 
-        label="Mùa Giải" 
-        icon="i-heroicons-globe-alt-20-solid" 
-        @click="() => router.push({
-          path: '/season',
-          query: { public: true }
-        })"
-        square
-      />
-      <UButton 
-        size="lg"
-        variant="ghost"
-        color="black" 
-        label="Danh sách Đội Bóng" 
-        icon="i-heroicons-user-group-20-solid" 
-        @click="() => router.push({
-          path: '/team',
-          query: { public: true }
-        })"
-        square
-      />
+      <div class="ml-8 mr-4 flex flex-col gap-2">
+        <div class="font-light mb-2 text-md">Chung</div>
+        <UButton 
+          size="lg"
+          variant="ghost"
+          color="white" 
+          label="Nhà" 
+          icon="i-heroicons-home-20-solid" 
+          @click="() => router.push('/')"
+          square
+        />
+        <UButton 
+          size="lg"
+          variant="ghost"
+          color="white" 
+          label="Mùa Giải" 
+          icon="i-heroicons-globe-alt-20-solid" 
+          @click="() => router.push({
+            path: '/season',
+            query: { public: true }
+          })"
+          square
+        />
+        <UButton 
+          size="lg"
+          variant="ghost"
+          color="white" 
+          label="Danh sách Đội Bóng" 
+          icon="i-heroicons-user-group-20-solid" 
+          @click="() => router.push({
+            path: '/team',
+            query: { public: true }
+          })"
+          square
+        />
 
-      <div class="font-light mb-2 text-md">Quản Lý</div>
-      <UButton 
-        size="lg"
-        variant="ghost"
-        color="black" 
-        label="Đội của tôi" 
-        icon="i-heroicons-users-20-solid" 
-        @click="() => router.push('/team/my')"
-        square
-      />
+        <div class="font-light mb-2 text-md">Quản Lý</div>
+        <UButton 
+          size="lg"
+          variant="ghost"
+          color="white" 
+          label="Đội của tôi" 
+          icon="i-heroicons-users-20-solid" 
+          @click="() => router.push('/team/my')"
+          square
+        />
+      </div>
     </div>
 
     <SidebarUserButton />

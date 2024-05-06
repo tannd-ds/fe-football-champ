cd5b32873fb8db94da37133bc88e5d63f2fbeccc
tannd-ds <donhattan1012dev@gmail.com>
2024-05-06 11:00:44 +0700

    add new color (toxic) and make it primary

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   ui: {
-    primary: 'yellow',
+    primary: 'toxic',
     gray: 'neutral'
   }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        toxic: {
+            50: '#f8ffe6',
+            100: '#f0ffc8',
+            200: '#dffe98',
+            300: '#c7f95d',
+            400: '#aeef2c',
+            500: '#95de0d',
+            600: '#6eab05',
+            700: '#548209',
+            800: '#44660e',
+            900: '#395611',
+            950: '#1c3003',
+        },
+      },
       textColor: {
         skin: {
           base: withOpacity("--color-text-base"),

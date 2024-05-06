92c15179a3294d4150e7a580cb9b3b13c4d170d6
tannd-ds <donhattan1012dev@gmail.com>
2024-05-06 11:04:23 +0700

    create UpcomingMatch in AdminDashboard

---
+++ components/Admin/UpcomingMatch.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- <div class="w-full flex gap-4 overflow-auto"> -->
+  <AppCard>
+
+    <div class="mb-8 text-3xl font-bold">Lịch Thi Đấu</div>
+    <div class="flex flex-col gap-2 max-h-[90%] overflow-auto divide-y divide-zinc-700">
+      <CMatchCard
+        v-for="(match, match_index) in all_matches"
+        :match_info="match"
+      />
+    </div>
+  </AppCard>
+  <!-- </div> -->
+</template>
+
+<script setup>
+
+const { data: all_matches } = await useFetch('http://localhost:8000/api/match/get/upcoming/');
+
+</script>
---
+++ components/C/match/card.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="group">
+    <div 
+      class="w-full h-full py-4 cursor-pointer peer"
+      @click="to_match_detail"
+    >
+      <div class="flex flex-col">
+        <div class="justify-between grid grid-cols-8 grid-rows-1 items-center gap-y-2">
+          <div class="col-span-2 flex flex-col items-center">
+            <div class="text-lg font-bold">{{ props.match_info.time_only }}</div>
+            <div class="text-sm text-gray-500">{{ props.match_info.date_only }}</div>
+          </div>
+          <div class="col-span-1 flex flex-col gap-2 items-center"></div>
+          <div class="col-span-2 flex flex-col gap-2 items-center">
+            <LazyUAvatar 
+              :src="`http://localhost:8000/api/get_img/team__${props.match_info.team_1_url_image}`"
+              alt="team_1_logo" 
+            />
+          </div>
+          <div class="col-span-1 flex flex-col gap-2 items-center">
+            <div class="font-bold text-gray-500">VS</div>
+          </div>
+          <div class="col-span-2 flex flex-col gap-2 items-center">
+            <LazyUAvatar 
+              :src="`http://localhost:8000/api/get_img/team__${props.match_info.team_2_url_image}`"
+              alt="team_1_logo" 
+            />
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  match_info: Object,
+})
+
+const router = useRouter();
+
+const to_match_detail = () => {
+  router.push(`/match/${props.match_info.id}`);
+}
+</script>

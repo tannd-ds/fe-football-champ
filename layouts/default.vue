c336fb17138992114b4623914725021886422540
tannd-ds <donhattan1012dev@gmail.com>
2024-05-07 18:00:11 +0700

    improve layouts design

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,13 @@
   <div class="flex items-center gap-4">
     <SidebarContainer />
 
-    <div class="w-screen h-screen mr-3 flex flex-row items-center justify-center">
+    <div class="w-screen h-screen mr-3 overflow-auto flex flex-row items-center justify-center">
       <slot></slot>
     </div>
   </div>
   <div class="w-screen h-screen -z-1 absolute top-0 left-0 -z-[1] overflow-hidden">
-    <div class="bg-cover w-screen h-screen"></div>
-    <div class="bg-overlay w-full h-full absolute top-0 left-0"></div>
+    <div class="bg-cover w-screen h-screen fixed top-0 left-0"></div>
+    <div class="bg-overlay w-full h-full fixed top-0 left-0"></div>
   </div>
 </template>
 
@@ -21,6 +21,6 @@
 
 .bg-overlay {
   background-color: #000;
-  opacity: 0.5;
+  opacity: 0.7;
 }
 </style>

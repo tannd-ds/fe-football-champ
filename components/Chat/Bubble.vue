ce41dd24d0386905cdb11936a2cf81042c1e3d68
ThachDepTrai <xuanthach220403@gmail.com>
2024-05-08 06:41:07 +0700

    be chat realtime

--- components/Chat/Bubble.vue
+++ components/Chat/Bubble.vue
@@ -1,31 +1,35 @@
-<template>
+<template >
     <div>
         {{cookie_usr_info.user_name}}
-        <!-- <ul>
-            <li v-for="msg in messeges" :key="msg">
-                <span> {{ msg.username }}: </span>
-                <span> {{ msg.messege }} </span>
+        <ul :key="messages.length">
+            <li v-for="msg in messages" :key="msg">
+                {{msg.username}}: {{msg.message}}
             </li>
-        </ul> -->
-        <input type="text" v-model="messege" />
+        </ul>
+        <input type="text" v-model="message" />
         <button @click="sendMessage">Send</button>
     </div>
 </template>
 <script setup>
-import { database,sref,set } from '../../plugins/firebase?t=null';
+import { database, sref, set,userMess } from '../../plugins/firebase?t=null';
+
+const messages = ref(userMess);
+
+/// thêm thông tin
+const { value: cookie_usr_info } = useCookie('usr_info');
+const message = ref('');
 function sendMessage() {
-    console.log(messege.value);
-    if (messege.value) {    
-        const newMessege = {
+    console.log(message.value);
+    if (message.value) {    
+        const newMessage = {
             username: cookie_usr_info.user_name,
-            messege: messege.value,
+            message: message.value,
         };
-        set(sref(database, 'messeges/' + cookie_usr_info.id+'/' + Date.now()), newMessege);
-        messege.value = '';
+        set(sref(database, 'messages/' + cookie_usr_info.id+'/' + Date.now()), newMessage);
+        message.value = '';
+        messages.value = userMess;
+        console.log(userMess);
+
     }
 };
-
-
-const { value: cookie_usr_info } = useCookie('usr_info');
-const messege = ref([]);
 </script>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,7 +1,8 @@
 
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref as sref, set } from "firebase/database";
-  
+import { getDatabase, ref as sref, set, onValue } from "firebase/database";
+const { value: cookie_usr_info } = useCookie('usr_info');
+
 const firebaseConfig = {
   databaseURL: "https://footballchamp-d2bec-default-rtdb.firebaseio.com",
 };
@@ -9,4 +10,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-export { database, sref, set };
+const get = sref(database, 'messages/' + cookie_usr_info.id);
+var userMess= []; 
+
+onValue(get, (snapshot) => {
+  userMess = [];
+  snapshot.forEach((value) => {
+    userMess.push(value.val());
+  });
+});
+export { database,sref, set,userMess };

5abe4b2d7b5197f512514a3cc08d00b099850fb9
tannd-ds <donhattan1012dev@gmail.com>
2024-05-09 10:11:13 +0700

    add teamLeader to AdminLeaderboard page
    Co-authored-by: Ndphuong-17 <dps17reds@gmail.com>

--- components/Admin/Dashboard.vue
+++ components/Admin/Dashboard.vue
@@ -2,6 +2,7 @@
   <div class="p-4 w-full h-full grid grid-cols-5 grid-rows-5 gap-4">
     <AdminNumber class="col-start-1 row-start-1 col-span-full"/>
     <AdminSeasonPieChart class="col-start-1 row-start-2 col-span-3 row-span-2"/>
+    <AdminTeamLeaderboard class="col-start-1 row-start-4 col-span-3 row-span-2"/>
     <AdminUpcomingMatch class="col-start-4 row-start-2 col-span-2 row-span-4"/>
   </div>
 </template>
---
+++ components/Admin/TeamLeaderboard.vue
@@ -0,0 +1,77 @@
+<template>
+  <AppCard class="w-full h-full">
+    <div class="w-full flex gap-2 overflow-auto">
+      <div v-for="season in all_seasons" :key = "season.id">
+        <UTooltip 
+          :text="season.name_season"
+          :popper="{ placement: 'top' }"
+        >
+          <div 
+            class="mb-2 px-4 py-2 w-56 cursor-pointer border border-zinc-700 rounded-lg truncate text-center"
+            :class="season.id === chosen_season_id ? 'bg-zinc-700' : 'bg-transparent'"
+            @click="chosen_season_id = season.id"
+            :title="season.name_season"
+          >
+            {{ season.name_season }}
+          </div>
+        </UTooltip>
+      </div>
+    </div>
+
+    <div>
+      <div 
+        v-if="teams_on_loading"
+        class="p-12 flex items-center justify-center"
+      >
+        <CLoadingIcon />
+      </div>
+
+      <div v-else>
+
+        <div 
+          v-if="teams_info.length > 0"
+          class="flex flex-col gap-4"
+        >
+          <div 
+            v-for="(team, team_index) in teams_info" 
+            :key="team.id"
+            class="p-3 flex items-center gap-1"
+          >
+            <div class="font-black text-3xl w-8">{{ team_index + 1 }}</div>
+            <LazyUAvatar :src="`http://localhost:8000/api/get_img/team__${team.url_image}`" />
+            <div class="grow">{{ team.name_team }}</div>
+            <div class="w-8">{{ team.total }}</div>
+          </div>
+
+        </div>
+
+        <div v-else>
+          <div class="p-12 text-center">Không có dữ liệu</div>
+        </div>
+
+      </div>
+
+    </div>
+  </AppCard>
+</template>
+
+<script setup>
+
+const all_seasons = await $fetch('http://localhost:8000/api/season/get_simple');
+
+const chosen_season_id = ref(all_seasons[0].id);
+
+const teams_info = ref([]);
+const teams_on_loading = ref(false);
+
+const fetch_teams = async () => {
+  teams_on_loading.value = true;
+  teams_info.value = await $fetch(`http://localhost:8000/api/match/listteam/${chosen_season_id.value}`);
+  // only show top 3
+  teams_info.value = teams_info.value.slice(0, 3);
+  teams_on_loading.value = false;
+}
+
+watch(() => chosen_season_id.value, fetch_teams, { immediate: true });
+
+</script>

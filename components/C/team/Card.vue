66eb27811c783b89ede18a4fe003e6831898fa2e
tannd-ds <donhattan1012dev@gmail.com>
2024-05-14 21:54:11 +0700

    add option button to TeamCard component

--- components/C/team/Card.vue
+++ components/C/team/Card.vue
@@ -1,9 +1,8 @@
 <template>
   <div
-    class="w-full aspect-square p-4 bg-zinc-900 hover:bg-zinc-800 rounded-lg bg-opacity-80
+    class="relative w-full aspect-square p-4 bg-zinc-900 rounded-lg bg-opacity-80
         border border-zinc-800
-        transition-all duration-200 ease-in-out cursor-pointer"
-    @click="on_click"
+        transition-all duration-200 ease-in-out"
     v-if="info.id != -1"
   >
     <div 
@@ -21,6 +20,16 @@
       >
         {{ info.name_team }}
       </span>
+    </div>
+
+    <div class="absolute right-2 top-2">
+      <UDropdown 
+        :items="manage_team_option" :popper="{ placement: 'bottom-start' }"
+      >
+        <div class="w-5 h-5 hover:bg-zinc-800 rounded">
+          <UIcon name="i-heroicons-ellipsis-vertical" class="w-5 h-5 text-gray-500" />
+        </div>
+      </UDropdown>
     </div>
 
   </div>
@@ -69,11 +78,37 @@ const props = defineProps({
 });
 
 const router = useRouter();
+const { value: cookie_usr_info } = useCookie('usr_info');
 
 const on_click = () => {
   if (props.info.id != -1) {
     router.push(`/team/${props.info.id}`);
   }
+}
+
+let manage_team_option = [
+  [{
+    label: 'Xem Thông Tin',
+    icon: 'i-heroicons-eye',
+    click: () => {
+      router.push(`/team/${props.info.id}`);
+    }
+  },
+  ]
+];
+
+if (cookie_usr_info.role === 1) {
+  manage_team_option[0].push({
+    label: 'Đá khỏi Giải',
+    icon: 'i-heroicons-trash',
+    click: () => {
+      console.log('Delete');
+      if (confirm('Bạn có chắc chắn muốn xóa đội bóng này khỏi giải đấu?')) {
+        console.log('Delete');
+        
+      }
+    }
+  });
 }
 
 </script>

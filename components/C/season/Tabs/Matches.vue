f53b11cc2d0d32b3ea42bbce1e3443aa44399d95
tannd-ds <donhattan1012dev@gmail.com>
2024-05-16 11:32:52 +0700

    fix display issues in CSeasonTabMatches

--- components/C/season/Tabs/Matches.vue
+++ components/C/season/Tabs/Matches.vue
@@ -5,10 +5,10 @@
       class="pr-3 w-full max-h-full overflow-auto flex flex-col gap-4"
     >
       <div 
-        v-for="(round, round_index) in matches_by_round"
+        v-for="(round, round_name) in matches_by_round"
         class="pr-3 w-full max-h-full overflow-auto flex flex-col gap-4"
       >
-        <div class="p-1 bg-gray-200 rounded text-zinc-800 text-center font-bold">Vòng {{ round_names[round_index] }}</div>
+        <div class="p-1 bg-gray-200 rounded text-zinc-800 text-center font-bold">{{ round_name }}</div>
           <CMatchItem
             v-for="(match, match_index) in round"
             :match="match"
@@ -31,7 +31,6 @@
 const route = useRoute();
 
 const all_matches = ref([]);
-const round_names = ref([]);
 const matches_by_round = ref({});
 
 const fetch_matches = async () => {
@@ -40,14 +39,11 @@ const fetch_matches = async () => {
   let new_round_names = [...new Set(new_all_matches.value.map((match) => match.round))];
   let new_matches_by_round = {};
   for (let i=0; i< new_round_names.length; i++) {
-    new_matches_by_round[new_round_names[i]] = new_all_matches.value.filter((match) => match.round == new_round_names[i]);
+    new_matches_by_round['Vòng ' + String(new_round_names[i])] = new_all_matches.value.filter((match) => match.round == new_round_names[i]);
   }
-
-  console.log('new matches comming!')
 
   return {
     all_matches: new_all_matches.value,
-    round_names: new_round_names,
     matches_by_round: new_matches_by_round,
   }
 }
@@ -56,7 +52,6 @@ const update_matches = async () => {
   let fetch_data = await fetch_matches();
 
   all_matches.value = fetch_data.all_matches;
-  round_names.value = fetch_data.round_names;
   matches_by_round.value = fetch_data.matches_by_round;
 }
 
